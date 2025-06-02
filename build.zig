e8fad1604a5debf599dd2f571a78138ee50ca4b3
Rafael Giuriatto <rafagiuriatto@hotmail.com>
2025-06-02 12:41:46 -0300

    zig 0.14

--- build.zig
+++ build.zig
@@ -2,23 +2,34 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
+    const name = "zigg";
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zigg",
-        .root_source_file = b.path("src/lib.zig"),
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("lib/root.zig"),
         .target = target,
         .optimize = optimize,
+    });
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe_mod.addImport(name, lib_mod);
+
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = name,
+        .root_module = lib_mod,
     });
 
     b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
-        .name = "zigg",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .name = name,
+        .root_module = exe_mod,
     });
 
     b.installArtifact(exe);
@@ -35,17 +46,13 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/lib.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = exe_mod,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
-const stdout = std.io.getStdOut().writer();
-const lib = @import("lib.zig");
+const lib = @import("zigg");
 
+const stdout = std.io.getStdOut().writer();
 const Base64 = lib.Base64;
 
 pub fn main() !void {
@@ -40,23 +40,29 @@ pub fn main() !void {
     t1.join();
     t2.join();
     t3.join();
+
+    try base64fn();
 }
 
 pub fn base64fn() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
-    const file = try std.fs.openFileAbsolute("/home/rawa/Dropbox/scrum.pdf", .{});
-    defer file.close();
-    const bytes = try file.readToEndAlloc(allocator, 5 * 1024 * 1024 * 1024);
-    defer allocator.free(bytes);
+    // const file = try std.fs.openFileAbsolute("/home/rawa/Dropbox/scrum.pdf", .{});
+    // defer file.close();
+    //
+    // const bytes = try file.readToEndAlloc(allocator, 5 * 1024 * 1024 * 1024);
+    // defer allocator.free(bytes);
 
     const base64 = Base64.init();
-    const in = try base64.encode(allocator, "Hi");
-    defer allocator.free(in);
 
+    const in = try base64.encode(allocator, "Hi");
     const out = try base64.decode(allocator, in);
-    defer allocator.free(out);
+
+    defer {
+        allocator.free(out);
+        allocator.free(in);
+    }
 
     try stdout.print("encoded: {s}\ndecoded: {s}\n", .{ in, out });
 }
